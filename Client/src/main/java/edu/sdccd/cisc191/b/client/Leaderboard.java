ee1b3c7ed0a04eaf0393f3a3fa59a124403532b5
Joaquin Dicang <jzxr05@gmail.com>
2021-04-09 20:40:41 -0700

    Updated for Friday: Leaderboard and User contain code that can be used for Discussion 9.2

--- Client/src/main/java/edu/sdccd/cisc191/b/client/Leaderboard.java
+++ Client/src/main/java/edu/sdccd/cisc191/b/client/Leaderboard.java
@@ -1,12 +1,23 @@
 package edu.sdccd.cisc191.b.client;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Leaderboard {
     private ArrayList<User> levelBoard;
     private ArrayList<User> scoreBoard;
 
     public Leaderboard(){
+        ArrayList<User> leaderboard = new ArrayList<User>();
+        leaderboard.add(new User("James"));
+        leaderboard.add(new User("Ronda", 15));
+        leaderboard.add(new User("Mary", 7));
+        leaderboard.add(new User("Marco", 11));
+
+        int total = 0;
+        for (User user: leaderboard) {
+            total += user.getUserLevel();
+        }
+        double avg = total/leaderboard.size();
 
     }
 
--- Client/src/main/java/edu/sdccd/cisc191/b/client/User.java
+++ Client/src/main/java/edu/sdccd/cisc191/b/client/User.java
@@ -1,12 +1,32 @@
 package edu.sdccd.cisc191.b.client;
 
-public class User {
+import java.util.*;
+
+public class User implements Comparable<User>{
     private String name;
     private int userXPLevel;
     private int userLevel;
     private int gamesPlayed;
     private int gameLevelsCleared;
     private int highScore;
+
+    public User(String name) {
+        this.name = name;
+        userLevel = 0;
+        userXPLevel = 0;
+        gamesPlayed = 0;
+        gameLevelsCleared = 0;
+        highScore = 0;
+    }
+
+    public User(String name, int userLevel) {
+        this.name = name;
+        this.userLevel = userLevel;
+        userXPLevel = 0;
+        gamesPlayed = 0;
+        gameLevelsCleared = 0;
+        highScore = 0;
+    }
 
     public String getName() {
         return name;
@@ -54,5 +74,30 @@ public class User {
 
     public void setHighScore(int highScore) {
         this.highScore = highScore;
+    }
+
+    @Override
+    public int compareTo(User user) {
+        int high = user.getHighScore();
+        int thisHigh = this.getHighScore();
+        int compVal = 0;
+        if (high == thisHigh) {
+            int level = user.getUserLevel();
+            int thisLevel = this.getUserLevel();
+            if (level == thisLevel) {
+                String name = user.getName();
+                String thisName = this.getName();
+                compVal = thisName.compareTo(name);
+            }
+            else if(level > thisLevel)
+                compVal = -1;
+            else if(level < thisLevel)
+                compVal = 1;
+        }
+        else if(high > thisHigh)
+            compVal = -1;
+        else if(high < thisHigh)
+            compVal = 1;
+        return compVal;
     }
 }
