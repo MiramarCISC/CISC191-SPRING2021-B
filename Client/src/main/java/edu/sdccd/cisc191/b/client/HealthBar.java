6be9f51d00cc10ed8a9ce433798d410ea61fc86b
Sholehani <10mariashol15@gmail.com>
2021-05-16 16:42:57 -0700

    Added the regenHealth method to HealthBar. Added PowerUp types "Immunity," "Regeneration," and default "Unknown" to PowerUp constructor. Added definitions for Bar methods.

--- Client/src/main/java/edu/sdccd/cisc191/b/client/HealthBar.java
+++ Client/src/main/java/edu/sdccd/cisc191/b/client/HealthBar.java
@@ -2,13 +2,57 @@ package edu.sdccd.cisc191.b.client;
 
 import java.awt.*;
 
-public class HealthBar extends Bar{
+public class HealthBar extends Bar {
+
+    //private long time;
 
     public HealthBar(int maxVal){
         super(maxVal,false,new Color(38,203,36));
     }
 
-    public void regenHealth(int perSecond, int time){
+    public void regenHealth(int healthIncrease, long regenTime){
+        long endSeconds = 0, currSeconds;
+        long startTime = System.currentTimeMillis();
 
+        // While there is time remaining and if the current health is
+        // not maxVal, increase current health .
+        while(endSeconds < regenTime){
+            currSeconds = endSeconds;
+
+            //Wait until the next second
+            while(endSeconds != currSeconds + 1) {
+                endSeconds = (System.currentTimeMillis() - startTime) / 1000;
+            }
+
+            if(getCurrentVal() <  getMaxVal())
+                increaseCurrent(healthIncrease);
+            if(getCurrentVal() > getMaxVal())
+                setCurrentVal(getMaxVal());
+
+            endSeconds = (System.currentTimeMillis() - startTime) / 1000;
+
+            //System.out.println("Seconds Passed: " + endSeconds);
+        }
     }
+
+
+
+
+    /*public long getTime() {
+        return time;
+    }*/
+
+
+    /*public static void main (String[] args) {
+        HealthBar hb = new HealthBar(50);
+        hb.decreaseCurrent(25);
+        System.out.println("Current Health: " + (hb.getCurrentVal()));
+
+        hb.regenHealth(2, 10);
+        System.out.println("Time passed: " + (hb.getTime()));
+        System.out.println("Current Health: " + (hb.getCurrentVal()));
+
+    }*/
 }
+
+
