76cfb607c1812f5ef3ce9a15e2270d5563bcda7d
gameTest <lkimdaryl@yahoo.com>
2021-05-24 20:12:42 -0700

    created place holders for username and score, and formatted how it will be displayed

--- Client/src/main/java/edu/sdccd/cisc191/b/client/GameView.java
+++ Client/src/main/java/edu/sdccd/cisc191/b/client/GameView.java
@@ -223,54 +223,126 @@ public class GameView  extends JPanel implements Runnable, MouseListener
                 g.drawString("Game Over", GameView_WIDTH / 2 - g.getFontMetrics().stringWidth("Game Over") / 2,
                         150);
 
+
+                //draws a square to encompass the leaderboard
                 g.setColor(Color.white);
                 g.drawRect(GameView_WIDTH / 4 - 3, 190, GameView_WIDTH / 2, 230);
 
+                //Title of the leaderboard
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 20));
                 g.drawString("Leaderboard", GameView_WIDTH / 4,
                         210);
 
+
+
+                String user1 = String.format("%.20s","Azaxar");
+                int score1 = 12345;
+
+
+                //Placeholders for top 10 highscores
+                //{
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("1. ", GameView_WIDTH / 4, 230);
+                g.drawString("1. " + user1, GameView_WIDTH / 4, 230);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("2. ", GameView_WIDTH / 4, 250);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 230);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("2. " + String.format("%-20s",user1), GameView_WIDTH / 4, 250);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("3. ", GameView_WIDTH / 4, 270);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 250);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("3. " + String.format("%-20s",user1), GameView_WIDTH / 4, 270);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("4. ", GameView_WIDTH / 4, 290);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 270);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("4. " + String.format("%-20s",user1), GameView_WIDTH / 4, 290);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("5. ", GameView_WIDTH / 4, 310);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 290);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("5. " + String.format("%-20s",user1), GameView_WIDTH / 4, 310);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("6. ", GameView_WIDTH / 4, 330);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 310);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("6. " + String.format("%-20s",user1), GameView_WIDTH / 4, 330);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("7. ", GameView_WIDTH / 4, 350);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 330);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("7. " + String.format("%-20s",user1), GameView_WIDTH / 4, 350);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("8. ", GameView_WIDTH / 4, 370);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 350);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("8. " + String.format("%-20s",user1), GameView_WIDTH / 4, 370);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("9. ", GameView_WIDTH / 4, 390);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 370);
+                //}
+
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("9. " + String.format("%-20s",user1), GameView_WIDTH / 4, 390);
 
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 15));
-                g.drawString("10. ", GameView_WIDTH / 4, 410);
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 390);
+                //}
 
+                //{
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString("10. " + String.format("%-20s",user1), GameView_WIDTH / 4, 410);
+
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Gameplay", Font.PLAIN, 15));
+                g.drawString(String.format("%d", score1), GameView_WIDTH / 2, 410);
+                //}
+
+
+                //Displays "Would you like to play again? Yes / No?"
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 30));
                 g.drawString("Would you like to play", GameView_WIDTH / 2
@@ -288,7 +360,11 @@ public class GameView  extends JPanel implements Runnable, MouseListener
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Gameplay", Font.PLAIN, 30));
                 g.drawString("No", 430, 550);
-            }
+
+
+
+
+            }//end of if(ingame)
         }//end of if(started)
 
         Toolkit.getDefaultToolkit().sync();
